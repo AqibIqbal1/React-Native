d780b81c992bd2417bb952261d26cf0af57eaec4
mraqib021 <mraqib021@gmail.com>
2024-06-03 05:42:16 +0500

    Hello World

---
+++ src/screens/hello world/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Button, Text, View} from 'react-native';
+
+const HelloWorld = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+      <Button title='press me !'></Button>
+    </View>
+  );
+};
+export default HelloWorld;
